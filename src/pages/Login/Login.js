a2382508bfa6df5e9ce115203802bccd21653918
kiprotichdominic <kiprotichdominic102@gmail.com>
2022-10-03 22:57:53 +0300

    add login with api

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,4 +1,27 @@
+import { useState } from "react"
+
 function Login() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+
+    async function login(e) {
+        e.preventDefault()
+        console.log(email, password);
+        let data = { email, password }
+        let result = await fetch("http://127.0.0.1:3000/auth/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify(data)
+        })
+        result = await result.json()
+        localStorage.setItem("token", result.token)
+        console.log(result.token);
+    }
+
     return (
         <div className="flex min-h-full flex-col justify-center py-12 sm:px-6 lg:px-8">
             <div className="sm:mx-auto sm:w-full sm:max-w-md">
@@ -20,6 +43,7 @@ function Login() {
                                     type="email"
                                     autoComplete="email"
                                     required
+                                    onChange={(e) => setEmail(e.target.value)}
                                     className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                                 />
                             </div>
@@ -36,34 +60,16 @@ function Login() {
                                     type="password"
                                     autoComplete="current-password"
                                     required
+                                    onChange={(e) => setPassword(e.target.value)}
                                     className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
                                 />
-                            </div>
-                        </div>
-
-                        <div className="flex items-center justify-between">
-                            <div className="flex items-center">
-                                <input
-                                    id="remember-me"
-                                    name="remember-me"
-                                    type="checkbox"
-                                    className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                                />
-                                <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
-                                    Remember me
-                                </label>
-                            </div>
-
-                            <div className="text-sm">
-                                <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                                    Forgot your password?
-                                </a>
                             </div>
                         </div>
 
                         <div>
                             <button
                                 type="submit"
+                                onClick={login}
                                 className="flex w-full justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                             >
                                 Sign in
